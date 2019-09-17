ab8c72d1f9bd1cd135868edaf38da53602237c0c
Abdulrahim Naser <abdulrahim.naser@hotmail.com>
2019-09-18 02:40:42 +0400

    Updated filling the Categories and Subcategories

--- src/Classes/Search.js
+++ src/Classes/Search.js
@@ -6,7 +6,8 @@ export class Search  extends Component{
 
     state={
         value: 0,
-        category: ""
+        category: "",
+        subcategory: ""
     }
     handleChange=(e)=>{
     this.setState({value:e.target.value});
@@ -14,16 +15,24 @@ export class Search  extends Component{
     handleCategory=(e)=>{
         this.setState({category:e});
         }
+    handleSubcategory=(e)=>{
+        this.setState({subcategory:e});
+        }
     render(){
     return  <form action="#" method="get" id="searchFrom" className="hide">
     <select value={this.state.value}  onChange={this.handleChange} className="custom-select">
         <option value="0">Your Destinations</option>
-        <option value="AbuDhabi">AbuDhabi</option>
+        <option value="AbuDhabi">Abu Dhabi</option>
+        <option value="AlAin">Al Ain</option>
         <option value="Dubai">Dubai</option>
         <option value="Sharjah">Sharjah</option>
+        <option value="Ajman">Ajman</option>
+        <option value="UAQ">Umm Al Qiween</option>
+        <option value="RAK">Ras Al Khaimah</option>
+        <option value="FUJ">Al Fujairah</option>
     </select>
-    <Categories value={this.state.value} choose={this.handleCategory} />
-   
+    <Categories value={this.state.value} choose={this.handleCategory} subchoose={this.handleSubcategory} />
+
     <Link to={`/listing/${this.state.category}`}>
 <button type="submit" className="btn dorne-btn"><i className="fa fa-search pr-2" aria-hidden="true"></i> Search</button></Link>
 </form>
@@ -34,10 +43,12 @@ export class Categories  extends Component{
 
     state={
         value: 0,
-        categories: []
+        subvalue: 0,
+        categories: [],
+        subcats: []
     }
     componentDidMount(){
-        fetch('https://guidestae.herokuapp.com/categories/get',{
+        fetch('https://guidestae.herokuapp.com/cats/get',{
         method: 'get',
         headers: new Headers({
             'Content-Type': 'application/json'
@@ -46,9 +57,10 @@ export class Categories  extends Component{
             res.json().then(r => {
                 this.setState({
                     categories: r.Categories,
-                    value:r.Categories[0].name
+                    value:r.Categories[0].uname
                 });
                 this.props.choose(this.state.value);
+                this.fillSubs(r.Categories[0].uname);
             }).catch(err =>{
                 this.setState({
                     loginstatus: false,
@@ -58,6 +70,90 @@ export class Categories  extends Component{
         });
     }
     handleChange=(e)=>{
+        this.setState({value:e.target.value});
+        this.props.choose(e.target.value);
+        this.fillSubs(e.target.value);
+    }
+    handleSubChange=(e)=>{
+        this.setState({subvalue:e.target.value});
+        this.props.subchoose(e.target.value);
+    }
+    fillSubs=(e)=>{    
+        fetch('https://guidestae.herokuapp.com/subcats/get/' + e,{
+                method: 'get',
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                })
+                }).then((res) => {
+                    res.json().then(r => {
+                        this.setState({
+                            subcats: r.Subcategories,
+                            subvalue:r.Subcategories[0].uname
+                        });
+                        this.props.subchoose(this.state.subvalue);
+                    }).catch(err =>{
+                        this.setState({
+                            loginstatus: false,
+                            showResultLogin: true
+                        });
+                    });
+                });
+        
+    }
+    render(){
+    return <div><select value={this.state.value}  onChange={this.handleChange} className="custom-select">
+       {
+           this.state.categories.map((category , i)=>{
+               return <option key={i} value={category.uname}>{category.name}</option>
+           })
+       }
+      
+    </select> 
+    <select value={this.state.subvalue}  onChange={this.handleSubChange} className="custom-select">
+       {
+           this.state.subcats.map((subcat , i)=>{
+               return <option key={i} value={subcat.uname}>{subcat.name}</option>
+           })
+       }
+      
+    </select>
+    </div>
+    }
+}
+
+export class Subcategories  extends Component{
+
+    state={
+        value: 0,
+        subcats: []
+    }
+    componentDidMount(){
+        console.log(this.props.value);
+        if (this.props.value != "") {
+
+            fetch('https://guidestae.herokuapp.com/subcats/get/' + this.props.value,{
+            method: 'get',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+            }).then((res) => {
+                res.json().then(r => {
+                    this.setState({
+                        subcats: r.Subcategories,
+                        value:r.Subcategories[0].uname
+                    });
+                    this.props.choose(this.state.value);
+                }).catch(err =>{
+                    this.setState({
+                        loginstatus: false,
+                        showResultLogin: true
+                    });
+                });
+            });
+        }
+    }
+
+    handleChange=(e)=>{
     this.setState({value:e.target.value});
     this.props.choose(e.target.value);
 
@@ -65,8 +161,8 @@ export class Categories  extends Component{
     render(){
     return <select value={this.state.value}  onChange={this.handleChange} className="custom-select">
        {
-           this.state.categories.map((category , i)=>{
-               return <option key={i} value={category.name}>{category.name}</option>
+           this.state.subcats.map((subcat , i)=>{
+               return <option key={i} value={subcat.uname}>{subcat.name}</option>
            })
        }
       
